827c5a27d29dc82475518052a984d61b86d275c2
DesertsP <junwen-pan@qq.com>
2019-06-10 14:27:47 +0800

    init

--- gpuidle.js
+++ gpuidle.js
@@ -27,7 +27,7 @@ else {
     window.location.href="signin.html";
 }
 
-var cql = 'select * from GPU_';
+var cql = 'select * from GPU_ order by -is_idle';
 
 AV.Query.doCloudQuery(cql, []).then(function (data) {
     var results = data.results;
